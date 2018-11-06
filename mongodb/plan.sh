47b861fe44ee79bb26d70840c0e1864ee86ae673
Siraj Rauff <siraj.rauff@indellient.com>
2018-11-06 11:11:53 -0500

    Restricting mongodb dependencies to version 3.2.10

--- mongodb/plan.sh
+++ mongodb/plan.sh
@@ -4,7 +4,7 @@ pkg_version=3.2.9
 pkg_description="MongoDB for the National Parks app"
 pkg_maintainer="The Chef Training Team <training@chef.io>"
 pkg_license=('AGPL-3.0')
-pkg_deps=(core/mongodb core/mongo-tools)
+pkg_deps=(core/mongodb/3.2.10 core/mongo-tools/3.2.10)
 
 pkg_svc_run="mongod --config $pkg_svc_config_path/mongod.conf"
 pkg_svc_user=hab
